340da65ffb2d98ee9c9d444e43ebe8af5eeb6c01
KarthikeyaDevarakonda <karthikeya8787@gmail.com>
2025-09-28 13:45:08 +0530

    added search filter and filter buttons in product page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -5,10 +5,8 @@ import { Link } from "react-router-dom";
 const RegisterPage = () => {
   return (
     <section className="h-fit sm:min-h-screen flex items-center justify-center bg-gradient-to-b from-pink-200 to-gray-100 sm:px-12 relative">
-      {/* Background Accent */}
       <div className="absolute inset-0 bg-[url('https://www.transparenttextures.com/patterns/diamond-upholstery.png')] opacity-20"></div>
 
-      {/* Card */}
       <div className="w-full max-w-md bg-white/70 backdrop-blur-xl sm:shadow-2xl sm:rounded-md p-8 sm:p-11 relative sm:z-10 sm:border-t-3 sm:border-slate-800">
         <h2 className="text-[27px] sm:text-3xl font-extrabold text-center text-slate-800 mb-4">
           ✨Create Account
@@ -18,11 +16,7 @@ const RegisterPage = () => {
           Join TrendCart and start exploring the latest fashion trends today.
         </p>
 
-        {/* Form */}
         <form className="flex flex-col gap-5">
-          {/* Full Name */}
-
-          {/* Email */}
           <div className="relative">
             <MdEmail className="absolute left-3 top-1/2 -translate-y-1/2 text-slate-400" />
             <input
@@ -32,7 +26,6 @@ const RegisterPage = () => {
             />
           </div>
 
-          {/* Password */}
           <div className="relative">
             <FaLock className="absolute left-3 top-1/2 -translate-y-1/2 text-slate-400" />
             <input
@@ -42,7 +35,6 @@ const RegisterPage = () => {
             />
           </div>
 
-          {/* Button */}
           <button
             type="submit"
             className="w-full bg-slate-800 hover:bg-slate-700 text-white font-semibold py-3 rounded-lg shadow-md transition"
@@ -51,7 +43,6 @@ const RegisterPage = () => {
           </button>
         </form>
 
-        {/* Login Link */}
         <p className="mt-6 text-center text-slate-600 text-sm">
           Already have an account?{" "}
           <Link
